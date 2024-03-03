fab98d288afe9d787e696596ede2cdea4bdf433f
Sivasurya007 <suryakarthika760@gmail.com>
2024-03-03 22:19:37 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router ,Route,Routes } from 'react-router-dom';
+import Page1 from './components/page1.jsx';
+import Page2 from './components/page2.jsx';
+import Page3 from './components/page3.jsx';
+
+
+
+
+function App() {
+  return ( 
+ 
+  <Router>
+    
+    <Routes>
+       <Route path="/" element={<Page1/>}/>
+       <Route path="/page2/:employeeId" element={<Page2 />} />
+       <Route path="/page3" element={<Page3/>}/>
+       
+
+    </Routes>
+ 
+    
+  </Router>
+
+  );
+}
+
+export default App;
---
+++ client/src/components/page3.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import '../styles/page3.scss';
+
+function EmployeeTable() {
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetchEmployees();
+    }, []);
+
+    const fetchEmployees = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/employees');
+            setEmployees(response.data);
+        } catch (error) {
+            console.error('Error fetching employees:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Employee Details</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Employee Name</th>
+                        <th>Employee ID</th>
+                        <th>Department</th>
+                        <th>Date of Birth</th>
+                        <th>Gender</th>
+                        <th>Designation</th>
+                        <th>Salary</th>
+                        <th>Phone Number</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(employee => (
+                        <tr key={employee.employeeidnumber}>
+                            <td>{employee.employeename}</td>
+                            <td>{employee.employeeidnumber}</td>
+                            <td>{employee.department}</td>
+                            <td>{new Date(employee.dateofbirth).toLocaleDateString()}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.designation}</td>
+                            <td>{employee.salary}</td>
+                            <td>{employee.phone_number}</td>
+                            <td>{employee.address}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default EmployeeTable;
+
