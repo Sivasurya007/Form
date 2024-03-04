e18d73da9ddc1c9c0669d06c8344c82ae3d63e29
Sivasurya007 <suryakarthika760@gmail.com>
2024-03-04 14:44:58 +0530

    shh

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://form-six-kohl.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+  
+
 const pool = new pg.Pool({
     user: 'uyulbcykylwyb2su07sf',
     host: 'b9khaerapmelvchjwdip-postgresql.services.clever-cloud.com',
